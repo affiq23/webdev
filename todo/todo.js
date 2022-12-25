3a8d6b092f142c2101e7e1a9b98e97376429e5e8
affiq23 <affiqem@gmail.com>
2022-12-24 20:28:11 -0600

    finished exercise

---
+++ todo/todo.js
@@ -0,0 +1,30 @@
+let todo = [];
+let val = true;
+let condition = prompt("What would you like to do?");
+
+while(condition !== "quit"){
+
+     if (condition === "list") {
+        for (let i = 0; i < todo.length; i++) {
+            console.log(i + " " + todo[i]);
+        }
+        console.log("********");
+    }
+
+    else if (condition === "new") {
+        let newTodo = prompt("Enter new to do");
+        todo.push(newTodo);
+        console.log(`${newTodo} added to list`);
+        console.log("********");
+    } 
+
+     else if(condition == "delete"){
+        let index = prompt("Enter index of list entry you want to delete.");
+        let deletedVal = todo.splice(index, 1);
+        console.log(`Deleted ${deletedVal}`)
+        
+        
+    }
+    condition = prompt("What would you like to do?");
+}
+console.log("You quit the app");
