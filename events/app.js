08d1a2aff83bc104cafb1039909b5af8391de67f
affiq23 <affiqem@gmail.com>
2023-07-08 14:36:37 -0500

    cleaned up folders

--- events/app.js
+++ events/app.js
@@ -7,5 +7,11 @@ const btn = document.querySelector("#v2");
     function scream(){
         console.log("AHHHHHHHHH");
     }
+     btn.onmouseenter = scream;
 
-    btn.onmouseenter = scream;
+
+    //event listener; allows for more than one function to be called
+    const btn3 = document.querySelector("#v3");
+    btn3.addEventListener("click", function(){
+        alert("clicked!");
+    })
