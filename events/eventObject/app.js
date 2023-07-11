d1ed8acd22d9e186dfdb5baa511343dbd71ff9c2
Affiq Mohammed <affiqem@gmail.com>
2023-07-11 13:11:08 -0500

    Update app.js
    semicolons

--- events/eventObject/app.js
+++ events/eventObject/app.js
@@ -1,18 +1,18 @@
 //7-11-23
 const button = document.querySelector("button");
 button.addEventListener('click', function (evt) { //evt is event object
-    alert("CLICK")
-    console.log(evt)
+    alert("CLICK");
+    console.log(evt);
 })
 
 const input = document.querySelector("input");
 input.addEventListener("keydown", function (evt) {
-    console.log(evt.key) //type of key
-    console.log(evt.code) //actual location of key on keyboard
+    console.log(evt.key); //type of key
+    console.log(evt.code); //actual location of key on keyboard
 })
 /*input.addEventListener("keyup", function(evt){
-    console.log("KEYUP")
-    console.log(evt)
+    console.log("KEYUP");
+    console.log(evt);
 })*/
 
 window.addEventListener("keydown", function (evt) { //doesn't need to just be in input box
