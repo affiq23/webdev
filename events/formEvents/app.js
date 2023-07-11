7abc43098f0f87f047c7c1f5b313bf195fd5527e
affiq23 <affiqem@gmail.com>
2023-07-11 17:35:46 -0500

    finished with events

--- events/formEvents/app.js
+++ events/formEvents/app.js
@@ -3,6 +3,7 @@ const form = document.querySelector("#shelterForm");
 const input = document.querySelector("#catname"); //whatever is typed into input field
 const list = document.querySelector("#cats");
 
+
 form.addEventListener("submit", function (evt) {
     evt.preventDefault(); //prevents form from doing its default behavior (moving to new page)
     const catName = input.value;
@@ -12,5 +13,12 @@ form.addEventListener("submit", function (evt) {
     input.value = ""; //clears the input value
 });
 
+list.addEventListener("click", function(e){
+    //nodeName makes sure you are clicking on li
+    e.target.nodeName == "LI" && e.target.remove(); //target makes sure you remove li you click on in the ul is removed
+})
 
-
+/*
+e.stopPropagation() is used to handle event bubbling; used in the same way as e.preventDefault() 
+didn't feel like writing whole code for it
+*/
