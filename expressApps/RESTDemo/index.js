9375a0ac0c5bbfdc0ae1d9cc21c04257362739a3
affiq23 <affiqem@gmail.com>
2023-08-08 00:05:01 -0500

    UUID

--- expressApps/RESTDemo/index.js
+++ expressApps/RESTDemo/index.js
@@ -1,32 +1,39 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { v4: uuid} = require("uuid");
+uuid();
 
 app.use(express.urlencoded({ extended: true })); //parses incoming requests with URL encoded payloads
 app.use(express.json()); //parses incoming requests with JSON payloads
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+
 const comments = [
     {
+        id: uuid(),
         username: "Todd",
         comment: "That is so funny"
     },
     {
+        id: uuid(),
         username: "Skyler",
         comment: "I like to go birdwatching"
     },
     {
+        id: uuid(),
         username: "Sk8terBoi",
         comment: "Plz delete your account Todd"
     },
     {
-        username: "slashingcove018",
+        id: uuid(),
+        username: "affiq23",
         comment: "I like to watch movies"
     }
 ]
 
-// CRUD - this is the READ portion; we are READING ALL THE COMMENTS
+// CRUD - this is the READ portion; reading all the comments
 app.get("/comments", (req, res) => {
     res.render("comments/index", { comments }); //have to pass comments through in order to use it in the ejs file
 });
@@ -38,11 +45,17 @@ app.get("/comments/new", (req, res) => {
 
 app.post("/comments", (req, res) => {
     const { username, comment } = req.body; //destructuring the object
-    comments.push({ username, comment }); //adds it to the array
-    res.send("It worked!");
+    comments.push({ username, comment, id: uuid()}); //adds it to the array
+    res.redirect("/comments"); //redirects as a GET
+});
+
+app.get("/comments/:id", (req, res) => {
+    const id = req.params.id;
+    const comment = comments.find(c => c.id === id); //to find the id in the comment
+    res.render("comments/show", { comment })
 })
 
-
+//IGNORE ALL THIS
 app.get("/tacos", (req, res) => {
     res.send("GET /tacos");
 });
